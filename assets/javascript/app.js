34f5b869a6c9fa639b97be7062ce86917e4844ab
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-22 21:38:00 -0700

    Test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,18 @@
+$(document).ready(function(){
+    console.log("ready");
+    
+
+
+
+
+
 $("#state").on("change",function(){
     console.log("worked");
     var something = $("#state").val();
     console.log(something);
 })
 
-I WROTE SOMETHING
+})
+
+
 
