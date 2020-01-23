5b6fd22002e3a54d6d182b4b39bb78e34557bf2e
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-22 22:39:59 -0700

    park data pull
    Pulls data from NPS API based on user state selaction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-    console.log("ready");
-    
+$(document).ready(function() {
+  //console.log("ready");
 
+  $("#state").on("change", function() {
+    let stateCode = $("#state").val();
+    //console.log(stateCode);
+    let queryURL =
+      "https://developer.nps.gov/api/v1/parks?stateCode=" +
+      stateCode +
+      "&q=National%20Park&api_key=mmnZ3oHc5B6EBEiihQUWhMb7QOocZRIgj8IploIN";
 
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+      })
 
-
-
-$("#state").on("change",function(){
-    console.log("worked");
-    var something = $("#state").val();
-    console.log(something);
-})
-
-})
-
-
-
+      .done(function(response){
+          console.log(response);
+          
+      })
+  });
+});
