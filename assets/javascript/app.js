8e93c9518a47695916bfed2dacd56f6c8c7cc4ec
Jordan Servos <jservos11@yahoo.com>
2020-01-25 00:36:20 -0700

    updated park pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,72 @@
 $(document).ready(function() {
-  //console.log("ready");
-
- 
- //Initialize Firebase 
-  let firebaseConfig = {
-    apiKey: "AIzaSyA7jX5P7grrZlBwV0QW49UM6OtH9FnXh34",
-    authDomain: "parksy-acb97.firebaseapp.com",
-    databaseURL: "https://parksy-acb97.firebaseio.com",
-    projectId: "parksy-acb97",
-    storageBucket: "parksy-acb97.appspot.com",
-    messagingSenderId: "75415393545",
-    appId: "1:75415393545:web:4ccbdca33052f72300efd8",
-    measurementId: "G-GYSCKSGM5V"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  //database reference as variable
-  let database = firebase.database
+    //console.log("ready");
 
 
+    //Initialize Firebase 
+    let firebaseConfig = {
+        apiKey: "AIzaSyA7jX5P7grrZlBwV0QW49UM6OtH9FnXh34",
+        authDomain: "parksy-acb97.firebaseapp.com",
+        databaseURL: "https://parksy-acb97.firebaseio.com",
+        projectId: "parksy-acb97",
+        storageBucket: "parksy-acb97.appspot.com",
+        messagingSenderId: "75415393545",
+        appId: "1:75415393545:web:4ccbdca33052f72300efd8",
+        measurementId: "G-GYSCKSGM5V"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 
-  
-
-  $("#state").on("change", function() {
-    let stateCode = $("#state").val();
-    //console.log(stateCode);
-    let queryURL =
-      "https://developer.nps.gov/api/v1/parks?stateCode=" +
-      stateCode +
-      "&q=National%20Park&api_key=mmnZ3oHc5B6EBEiihQUWhMb7QOocZRIgj8IploIN";
-
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      })
-
-      .done(function(response){
-          console.log(response);
-          let results = response.data;
-          //console.log(results);
-          let randomPark = Math.floor(Math.random() * results.length);
-          //console.log(randomPark);
-          let featuredPark = results[randomPark];
-          console.log(featuredPark);
-          let cardTitle = featuredPark.fullName;
-          console.log(cardTitle);
-          let cardText = featuredPark.description;
-          console.log(cardText); 
-
-          $(".card-title").empty();
-          $(".card-title").text(cardTitle);
-          $(".card-text").text(cardText);
-          
-//need to get different parks into each card
-          
-          
-          
-          
-          
+    //database reference as variable
+    let database = firebase.database
 
 
-          
 
-          
-      })
-  });
+
+
+    $("#state").on("change", function() {
+        let stateCode = $("#state").val();
+        //console.log(stateCode);
+        let queryURL =
+            "https://developer.nps.gov/api/v1/parks?stateCode=" +
+            stateCode +
+            "&q=National%20Park&api_key=mmnZ3oHc5B6EBEiihQUWhMb7QOocZRIgj8IploIN";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+        .done(function(response) {
+            console.log(response);
+            let results = response.data;
+            //console.log(results);
+            let randomPark = Math.floor(Math.random() * results.length);
+            //console.log(randomPark);
+            let featuredPark = results[randomPark];
+            console.log(featuredPark);
+            let cardTitle = featuredPark.fullName;
+            console.log(cardTitle);
+            let cardText = featuredPark.description;
+            console.log(cardText);
+
+            $(".card-title").empty();
+            $(".card-title").text(cardTitle);
+            $(".card-text").text(cardText);
+
+            //need to get different parks into each card
+
+
+
+
+
+            $(function() {
+                $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+            });
+
+
+
+
+        })
+    });
 });
