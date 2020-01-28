c9298f8a19037a19b86403cec8b59216f305397e
RubixUnderlord <57545547+RubixUnderlord@users.noreply.github.com>
2020-01-27 21:14:02 -0700

    Working Now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //console.log("ready");
 
     //Initialize Firebase
@@ -15,19 +15,27 @@ $(document).ready(function() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     // firebase.analytics();
-
+    console.log("Firebase initialized");
     //database reference as variable
     let database = firebase.database();
     let stateCode = sessionStorage.getItem("stateCode");
-    
+    let likeCounter = 0;
     let parkName;
     let parkDescription;
     let parkImage;
-    let likes = 0;
     let allStateCords = ["61.370716,-152.404419", "34.969704,-92.373123", "33.729759,-111.431221", "36.116203,-119.681564", "39.059811,-105.311104", "27.766279,-81.686783", "21.094318,-157.498337", "39.849426,-86.258278", "37.668140,-84.670067", "44.693947,-69.381927", "43.326618,-84.536095", "45.694454,-93.900192", "38.456085,-92.288368", "46.921925,-110.454353", "38.313515,-117.055374", "34.840515,-106.248482", "47.528912,-99.784012", "40.388783,-82.764915", "44.572021,-122.070938", "33.856892,-80.945007", "44.299782,-99.438828", "35.747845,-86.692345", "31.054487,-97.563461", "40.150032,-111.862434", "37.769337,-78.169968", "47.400902,-121.490494", "42.755966,-107.302490"];
+
+    console.log("variables set, ALL State Coordinates:", allStateCords);
+
+    var webCamNum = allStateCords[sessionStorage.getItem("stateNumber")];
+    console.log("Web Cam Number", webCamNum);
+
+    var webCam =
+        "https://api.windy.com/api/webcams/v2/list/nearby=" + webCamNum + ",250?key=PZcbLAY0Dop4Gbuyc9g6EHlASwBQW9SJ";
+
     populatePage();
 
-    $("#state").on("change", function() {
+    $("#state").on("change", function () {
         codeNumber = $("#state").val();
         parkNumber = codeNumber.replace(/^\D+/g, '');
         stateCode = codeNumber.replace(/[0-9]/g, '')
@@ -40,7 +48,7 @@ $(document).ready(function() {
     });
 
     //like function
-    $("#like-btn").on("click", function(event) {
+    $("#like-btn").on("click", function (event) {
         event.preventDefault();
         parkName = $("#park-title").val().trim();
         console.log(parkName);
@@ -49,32 +57,27 @@ $(document).ready(function() {
 
     });
 
-});
 
-//where the webcam gets add to the page
-var webCamNum = allStateCords[sessionStorage.getItem("stateNumber")];
-// console.log(webCamNum);
-var webCam =
-    "https://api.windy.com/api/webcams/v2/list/nearby=" + webCamNum + ",250?key=PZcbLAY0Dop4Gbuyc9g6EHlASwBQW9SJ";
+    $.ajax({
+        url: webCam,
+        method: "GET"
+    }).then(function (response) {
+        //console.log(response);
+        $("#liveWebcam").attr(
+            "src",
+            "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[0].id + "/day"
+        );
+        $("#liveWebcam1").attr(
+            "src",
+            "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[1].id + "/day"
+        );
+        $("#liveWebcam2").attr(
+            "src",
+            "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[2].id + "/day"
+        );
+    });
 
-$.ajax({
-    url: webCam,
-    method: "GET"
-}).then(function(response) {
-    //console.log(response);
-    $("#liveWebcam").attr(
-        "src",
-        "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[0].id + "/day"
-    );
-    $("#liveWebcam1").attr(
-        "src",
-        "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[1].id + "/day"
-    );
-    $("#liveWebcam2").attr(
-        "src",
-        "https://webcams.windy.com/webcams/public/embed/player/" + response.result.webcams[2].id + "/day"
-    );
-});
+
 
 
 function populatePage() {
@@ -86,10 +89,10 @@ function populatePage() {
         "&q=National%20Park&api_key=mmnZ3oHc5B6EBEiihQUWhMb7QOocZRIgj8IploIN";
 
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .done(function(response) {
+        url: queryURL,
+        method: "GET"
+    })
+        .done(function (response) {
             let results = response.data;
             console.log(response);
             //console.log(results);
@@ -107,7 +110,6 @@ function populatePage() {
         });
 }
 
-
 var stateArray = ["CO", "MN", "OH", "NY", "NM"];
 for (i = 0; i < stateArray.length; i++) {
     let stateCode = stateArray[i];
@@ -118,10 +120,10 @@ for (i = 0; i < stateArray.length; i++) {
         "&q=National%20Park&api_key=mmnZ3oHc5B6EBEiihQUWhMb7QOocZRIgj8IploIN";
 
     $.ajax({
-            url: featuredURL,
-            method: "GET"
-        })
-        .done(function(response) {
+        url: featuredURL,
+        method: "GET"
+    })
+        .done(function (response) {
             console.log(response);
             let results = response.data;
             //console.log(results);
@@ -148,3 +150,4 @@ for (i = 0; i < stateArray.length; i++) {
 
         });
 }
+});
